eaca97c24136a7dfea121384e8f44e2baf96a65e
smkrep <m2101760@edu.misis.ru>
2022-11-22 01:25:05 +0300

    added test code for gnuplot/matplot

--- prj.lab/matplotproj/matplotproj_test.cpp
+++ prj.lab/matplotproj/matplotproj_test.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+//#include <iostream>
+//#include <matplot/matplot.h>
+//
+//int main()
+//{
+//	return 0;
+//}
+
+#include <cmath>
 #include <matplot/matplot.h>
 
-int main()
-{
-	return 0;
+int main() {
+    using namespace matplot;
+    std::vector<double> x = linspace(0, 2 * pi);
+    std::vector<double> y = transform(x, [](auto x) { return sin(x); });
+
+    plot(x, y, "-o");
+    hold(on);
+    plot(x, transform(y, [](auto y) { return -y; }), "--xr");
+    plot(x, transform(x, [](auto x) { return x / pi - 1.; }), "-:gs");
+    plot({ 1.0, 0.7, 0.4, 0.0, -0.4, -0.7, -1 }, "k");
+    show();
+    return 0;
 }
